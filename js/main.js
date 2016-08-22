96f1cd80b5a72d3868d17563c2f1679ab2816c85
hardyj1 <hardyj1@seattleu.edu>
2016-08-22 18:08:03 -0400

    finished all assignment basic requirements

--- js/main.js
+++ js/main.js
@@ -30,15 +30,7 @@ $( document ).ready(function() {
       $('.user-info').show(); 
     });
 
-    // TODO: Create a function to listen for clicks on all the "View Details"
-    // buttons so that when a user clicks a "View Details" button they see
-    // the content contained in the elements with the class "details" in the
-    // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
+    
   $('.view-details').on('click', function(event){
     console.log(event);
     var targetElement = event.target;
@@ -55,16 +47,6 @@ $( document ).ready(function() {
     });
 });
 
-
-    // TODO: Create a function that listens for clicks on the voting buttons and
-    // looks at the `data-vote` attribute on each button to see what was voted for,
-    // then determines the updated vote breakdown to adjust the progress bars.
-    //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
 
 // Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
